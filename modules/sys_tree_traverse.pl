da151553ec721aa15a5e34ba115b8e14d0d048d1
Harald Hope <smxi@users.noreply.github.com>
2017-10-29 15:04:19 -0700

    Added files to modules.

---
+++ modules/sys_tree_traverse.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+use File::Find;
+use strict;
+# use warnings;
+use 5.010;
+my @content = (); 
+find( \&wanted, "/sys");
+process_data( @content );
+sub wanted {
+	return if -d; # not directory
+	return unless -e; # Must exist
+	return unless -r; # Must be readable
+	return unless -f; # Must be file
+	# note: a new file in 4.11 /sys can hang this, it is /parameter/ then
+	# a few variables. Since inxi does not need to see that file, we will
+	# not use it. Also do not need . files or __ starting files
+	return if $File::Find::name =~ /\/(\.[a-z]|__|parameters\/|debug\/)/;
+	# comment this one out if you experience hangs or if 
+	# we discover syntax of foreign language characters
+	return unless -T; # Must be ascii like
+	# print $File::Find::name . "\n";
+	push @content, $File::Find::name;
+	return;
+}
+sub process_data {
+	my $result = "";
+	my $row = "";
+	my $fh;
+	my $data="";
+	my $sep="";
+	# no sorts, we want the order it comes in
+	# @content = sort @content; 
+	foreach (@content){
+		$data="";
+		$sep="";
+		open($fh, "<$_");
+		while ($row = <$fh>) {
+			chomp $row;
+			$data .= $sep . "\"" . $row . "\"";
+			$sep=", ";
+		}
+		$result .= "$_:[$data]\n";
+		# print "$_:[$data]\n"
+	}
+	# print scalar @content . "\n";
+	print "$result";
+}
